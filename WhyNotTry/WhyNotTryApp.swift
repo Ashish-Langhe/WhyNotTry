3e1ac080b7369210041b2c209f8ef6e0c6223815
Ashish Langhe <ashish.langhe@globant.com>
2023-11-02 14:31:51 +0530

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Ashish Langhe on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
